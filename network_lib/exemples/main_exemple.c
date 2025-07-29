fcf1fa565508bf15407cdce4568a90948908d78e
Lilmy <matheo.moinache@epitech.eu>
2025-07-29 16:59:13 +0400

    FIX: copy main to exemple_main

--- network_lib/exemples/main_exemple.c
+++ network_lib/exemples/main_exemple.c
@@ -1,12 +1,15 @@
 /*
 ** EPITECH PROJECT, 2025
-** main
+** network_lib
 ** File description:
-** main
+** server
 */
 
-#include "include/net.h"
-#include "include/log.h"
+#include "../include/net.h"
+#include "../include/log.h"
+#include <signal.h>
+
+static bool is_running = true;
 
 void handle_client_connect(net_client_t *client, net_server_t *server, void *args)
 {
@@ -29,10 +32,20 @@ void handle_client_disconnect(net_client_t *client, net_server_t *server, void *
 void handle_client_data(net_client_t *client, net_server_t *server, void *args)
 {
     char *cmd = (char *)args;
-
-    (void)server;
+    msg_packet_t packet = {
+        .message = "a client send data\n",
+        .message_size = strlen("a client send data\n"),
+    };
+    net_send_all(server, packet);
     LOG_INFO("Data from fd=%d: %s", client->fd, client->buffer);
     LOG_INFO("CMD: %s", cmd);
+}
+
+void sighandler(int sig)
+{
+    signal(sig, SIG_IGN);
+    printf("Server has been shut down.\n");
+    is_running = false;
 }
 
 int main(void)
@@ -45,16 +58,18 @@ int main(void)
 
     if (!server)
         return 84;
-    set_handle_connection(server, handle_client_connect, data_connect_command);
-    set_handle_disconnection(server, handle_client_disconnect, data_disconnect_command);
-    set_handle_data(server, handle_client_data, data_command);
-
+    net_set_handle_connection(server, handle_client_connect, data_connect_command);
+    net_set_handle_disconnection(server, handle_client_disconnect, data_disconnect_command);
+    net_set_handle_data(server, handle_client_data, data_command);
     // set_handle_connection(server, NULL);
     // set_handle_disconnection(server, NULL);
     // set_handle_data(server, NULL);
     if (!net_server_start(server))
         return 84;
     while (server->running) {
+        signal(SIGINT, sighandler);
+        if (!is_running)
+            server->running = false;
         net_server_poll(server, timeout);
     }
     net_server_destroy(server);
